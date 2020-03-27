2afd5a27751042c1d8d4bd0e68eefd6bf161a421
anandia <anandiamy@gmail.com>
2020-03-27 20:38:24 +0700

    hapus material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import {NgModule} from '@angular/core'
 
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {LoginComponent} from './login/login.component'
-import {MaterialModule} from './core/material.module'
 import {FormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http';
 import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
 import { Error404Component } from './error404/error404.component'
-import {MatListModule} from '@angular/material/list'
 
 @NgModule({
     declarations: [
@@ -24,11 +21,8 @@ import {MatListModule} from '@angular/material/list'
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule,
-        MaterialModule,
         HttpClientModule,
         FormsModule,
-        MatListModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
